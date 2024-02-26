3beb89eaa74715711eea166bad754e9b23437768
Uladzimir Shytsko <92379291+shytsko@users.noreply.github.com>
2024-02-26 09:43:32 +0300

    Add commens to task4.cpp

--- part3/homework2/task4/task4.cpp
+++ part3/homework2/task4/task4.cpp
@@ -22,49 +22,63 @@
 #include <fstream>
 #include <iostream>
 
-#define DENOM_NUMB 6
-#define ATM_CAPACITY 1000
+#define DENOM_NUMB 6         // Количество номиналов банкнот
+#define ATM_CAPACITY 1000    // Количество банкнот, которое можно загрузить а банкомат
 
-#define OK 0
-#define BAD_FORMAT -1
-#define CRC_ERROR -2
-#define NO_FILE -3
+// Коды результатов сохранения в фал или чтения из файла 
+#define OK 0              // Успешно
+#define BAD_FORMAT -1     // Не верный формат файла 
+#define CRC_ERROR -2      // Ошибка контрольой суммы
+#define NO_FILE -3        // Нет файла
 
+// Формат файла:
+// Бинарный формат. Сначала расположени массив типа int, размер массива указывается. 
+// В конце контролльная сумма типа int, контрольная сумма равна сумме элементов массива
+
+// Чтение данных из файла
+// filePath - имя файла
+// data - масив, в который необходимо сохранить данные
+// size - размер массива
 int loadData(const char *filePath, int *data, size_t size) {
   int rerutn_code = OK;
   std::ifstream file(filePath, std::ios::binary);
-  if (file.is_open()) {
-    file.read((char *)data, size * sizeof(int));
-    if (file.gcount() == size * sizeof(int)) {
-      int crc;
-      file.read((char *)&crc, sizeof(int));
-      if (file.gcount() == sizeof(int)) {
+  if (file.is_open()) {                            // Проверка, открыт ли файл
+    file.read((char *)data, size * sizeof(int));   // Чтение массива
+    if (file.gcount() == size * sizeof(int)) {     // Проверка количества считанных байт
+      int crc;  
+      file.read((char *)&crc, sizeof(int));        // чтение контрольной суммы
+      if (file.gcount() == sizeof(int)) {          // Проверка количества считанных байт
         int sum = 0;
-        for (size_t i = 0; i < size; i++)
+        for (size_t i = 0; i < size; i++)          // Подсчет суммы элементов считаного массива
           sum += *(data + i);
-        if (sum != crc)
-          rerutn_code = CRC_ERROR;
+        if (sum != crc)                            // Проверка контрольной суммы
+          rerutn_code = CRC_ERROR;                 // Ошибка при подсчете контрольной суммы
       } else {
-        rerutn_code = BAD_FORMAT;
+        rerutn_code = BAD_FORMAT;                  // Ошибка при чтении основных данных
       }
     } else {
-      rerutn_code = BAD_FORMAT;
+      rerutn_code = BAD_FORMAT;                    // Ошибка при чтении контрольной суммы
     }
   } else {
-    rerutn_code = NO_FILE;
+    rerutn_code = NO_FILE;                         // Ошибка, если файла нет
   }
 
   return rerutn_code;
 }
 
+
+// Запись файла
+// filePath - имя файла
+// data - масив с данными
+// size - размер массива
 int saveData(const char *filePath, const int *data, size_t size) {
   std::ofstream file(filePath, std::ios::binary);
-  if (file.is_open()) {
+  if (file.is_open()) {                              // Проверка, открыт ли файл
     int crc = 0;
-    for (size_t i = 0; i < size; i++)
+    for (size_t i = 0; i < size; i++)                // Подсчет контрольной суммы
       crc += *(data + i);
-    file.write((char *)data, size * sizeof(int));
-    file.write((char *)&crc, sizeof(int));
+    file.write((char *)data, size * sizeof(int));    // Запись массива
+    file.write((char *)&crc, sizeof(int));           // Запись контрольной суммы
     file.close();
   } else {
     return NO_FILE;
@@ -72,6 +86,11 @@ int saveData(const char *filePath, const int *data, size_t size) {
   return OK;
 }
 
+
+// Вывод состояния банкомата
+// data - массив с количеством банкнот каждого номинала
+// denom - массив с номиналами банкнот
+// size - размер массивов data и denom
 void printData(const int *data, const int *denom, size_t size) {
   int totalAmount = 0;
   int totalBanknotes = 0;
@@ -88,14 +107,14 @@ void printData(const int *data, const int *denom, size_t size) {
 
 int main() {
   const char *filePath = "atm.bin";
-  int denominations[] = {5000, 2000, 1000, 500, 200, 100};
+  int denominations[] = {5000, 2000, 1000, 500, 200, 100};  // массив с номиналами
 
-  int numberBanknotes[DENOM_NUMB];
+  int numberBanknotes[DENOM_NUMB];                          // массив с количеством банкнот каждого номинала
 
-  int res = loadData(filePath, numberBanknotes, DENOM_NUMB);
+  int res = loadData(filePath, numberBanknotes, DENOM_NUMB);  // загрузка из файла
 
-  if (res == NO_FILE) {
-    for (size_t i = 0; i < DENOM_NUMB; i++)
+  if (res == NO_FILE) {                                       // если файла не существует
+    for (size_t i = 0; i < DENOM_NUMB; i++)                   // инициализация нулями (банкомат пустой)
       numberBanknotes[i] = 0;
   } else if (res == BAD_FORMAT || res == CRC_ERROR) {
     std::cout << "ATM malfunction!" << std::endl;
@@ -111,12 +130,12 @@ int main() {
   char mode;
   std::cout << "Mode ('+' - refill, '-' - cash): ";
   std::cin >> mode;
-  if (mode == '+') {
+  if (mode == '+') {                                              // режим заполнения
     std::srand(std::time(nullptr));
-    for (int i = numberBanknotesTotal; i < ATM_CAPACITY; i++)
+    for (int i = numberBanknotesTotal; i < ATM_CAPACITY; i++)     // заполнение случайными банкнотами до максимального количества
       numberBanknotes[std::rand() % DENOM_NUMB]++;
 
-    res = saveData(filePath, numberBanknotes, DENOM_NUMB);
+    res = saveData(filePath, numberBanknotes, DENOM_NUMB);        // сохранение данных в файл
     if (res == OK) {
       std::cout << "ATM is full" << std::endl;
     } else {
@@ -124,28 +143,28 @@ int main() {
       exit(-1);
     }
 
-  } else if (mode == '-') {
+  } else if (mode == '-') {                                        // режим выдачи
     std::cout << "Amount for cash: ";
     int amount;
-    std::cin >> amount;
-    if (amount % 100 != 0) {
+    std::cin >> amount;                                            // ввод суммы для выдачи
+    if (amount % 100 != 0) {                                       // проверка суммы для выдачи на кратность минимальному номиналу
       std::cout << "The amount must be a multiple of 100" << std::endl;
       exit(-1);
     }
-    int numberBanknotesForCash[DENOM_NUMB] = {0, 0, 0, 0, 0, 0};
-    int remainingAmount = amount;
-    for (int i = 0; remainingAmount > 0 && i < DENOM_NUMB; i++) {
-      numberBanknotesForCash[i] =
-          std::min(remainingAmount / denominations[i], numberBanknotes[i]);
+    int numberBanknotesForCash[DENOM_NUMB] = {0, 0, 0, 0, 0, 0};    // массив с количество банкнот для выдачи
+    int remainingAmount = amount;                        
+    for (int i = 0; remainingAmount > 0 && i < DENOM_NUMB; i++) {              // набор банкнот каждого номинала для выдачи
+      numberBanknotesForCash[i] =                                              // количество определяется как минимальное из остатка от деления 
+          std::min(remainingAmount / denominations[i], numberBanknotes[i]);    // оставшейся суммы и количества банкнот в банкомате
       remainingAmount -= denominations[i] * numberBanknotesForCash[i];
     }
 
-    if (remainingAmount != 0) {
-      std::cout << "It is impossible to issue this amount" << std::endl;
+    if (remainingAmount != 0) {                                                // если оставшаяся сумма не равна нулю
+      std::cout << "It is impossible to issue this amount" << std::endl;       // выдать запрошенную сумму невозможно
       exit(-1);
     }
 
-    int checkAmount = 0;
+    int checkAmount = 0;                                                       // контрольна проверка 
     for (int i = 0; i < DENOM_NUMB; i++)
       checkAmount += numberBanknotesForCash[i] * denominations[i];
     if (checkAmount != amount) {
@@ -153,12 +172,12 @@ int main() {
       exit(-1);
     }
 
-    for (int i = 0; i < DENOM_NUMB; i++) {
+    for (int i = 0; i < DENOM_NUMB; i++) {                         // корректировка остатка банкнот
       numberBanknotes[i] -= numberBanknotesForCash[i];
     }
 
-    if (saveData(filePath, numberBanknotes, DENOM_NUMB) == OK) {
-      for (int i = 0; i < DENOM_NUMB; i++)
+    if (saveData(filePath, numberBanknotes, DENOM_NUMB) == OK) {   // сохранение данных и провека
+      for (int i = 0; i < DENOM_NUMB; i++)                          // если сохранено успешно, выдача наличных
         if (numberBanknotesForCash[i])
           std::cout << numberBanknotesForCash[i] << " banknotes of "
                     << denominations[i] << "rubles were issued" << std::endl;
