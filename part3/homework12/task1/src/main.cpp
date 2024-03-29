82efdd0cafbce57e2fe62c2d8f4e8f7671fe81a9
Vladimir Shytsko <shytsko.v@gmail.com>
2024-03-29 17:58:31 +0300

    Done task 1

--- part3/homework12/task1/src/main.cpp
+++ part3/homework12/task1/src/main.cpp
@@ -1,14 +1,44 @@
-#include <iostream>
 #include <cpr/cpr.h>
+#include <iostream>
+#include <string>
 
-int main()
-{
-    cpr::Response r = cpr::Get(cpr::Url{"http://www.httpbin.org/get"});
+void printResponse(cpr::Response &response) {
+  std::cout << response.url << std::endl;
+  std::cout << response.status_code << std::endl;
+  std::cout << response.header["content-type"] << std::endl;
+  std::cout << response.text << std::endl;
+}
 
-    std::cout << r.url << std::endl;                    // http://www.httpbin.org/get
-    std::cout << r.status_code << std::endl;            // 200
-    std::cout << r.header["content-type"] << std::endl; // application/json
-    std::cout << r.text << std::endl;
+int main() {
+  std::string baseURL = "http://httpbin.org";
 
-    return 0;
+  while (true) {
+    std::string command;
+    std::cout
+        << "Enter request type (get, post, put, delete, patch) or exit > ";
+    std::cin >> command;
+
+    if (command == "exit")
+      break;
+
+    cpr::Response response;
+    if (command == "get") {
+      response = cpr::Get(cpr::Url{baseURL + "/get"});
+    } else if (command == "post") {
+      response = cpr::Post(cpr::Url{baseURL + "/post"});
+    } else if (command == "put") {
+      response = cpr::Put(cpr::Url{baseURL + "/put"});
+    } else if (command == "delete") {
+      response = cpr::Delete(cpr::Url{baseURL + "/delete"});
+    } else if (command == "patch") {
+      response = cpr::Patch(cpr::Url{baseURL + "/patch"});
+    } else {
+      std::cout << "Invalid command" << std::endl;
+      continue;
+    }
+
+    printResponse(response);
+  }
+
+  return 0;
 }
