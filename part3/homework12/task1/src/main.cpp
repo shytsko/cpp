ba7a86f42a478206cace9c37263ee1d4c5edd1fa
Vladimir Shytsko <shytsko.v@gmail.com>
2024-03-29 16:49:32 +0300

    Update

--- part3/homework12/task1/src/main.cpp
+++ part3/homework12/task1/src/main.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
+#include <cpr/cpr.h>
 
-int main() { return 0; }
+int main()
+{
+    cpr::Response r = cpr::Get(cpr::Url{"http://www.httpbin.org/get"});
+
+    std::cout << r.url << std::endl;                    // http://www.httpbin.org/get
+    std::cout << r.status_code << std::endl;            // 200
+    std::cout << r.header["content-type"] << std::endl; // application/json
+    std::cout << r.text << std::endl;
+
+    return 0;
+}
